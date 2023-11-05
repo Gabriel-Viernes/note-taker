75651facd946b17ea3f32607c5f3a989e02d2ae2
Gabriel Viernes <viernesgabriel@gmail.com>
2023-11-05 15:37:52 -0600

    added working api GET route

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ const express = require('express')
 const fs = require('fs')
 const path = require('path')
 const port = 3000;
+// const api = require('./routes/api.js')
 
 const server = express();
 server.use(express.static(path.join(__dirname, 'public'), {extensions:['html']}))
+// server.use('/api', api)
+server.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        if(err) {
+            console.log(err)
+        } else {
+            res.json(JSON.parse(data))
+        }
+    })
+})
+
+
 
 server.listen(port, () => {
     console.log('Server started')
