5a609df8334373d9e9cdef47a7b5f29cf3f8ba69
Gabriel Viernes <viernesgabriel@gmail.com>
2023-11-05 16:59:50 -0600

    imported uuid and added post route for /api/notes

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express')
 const fs = require('fs')
 const path = require('path')
 const port = 3000;
-// const api = require('./routes/api.js')
+const {v4: uuidv4} = require('uuid')
 
 const server = express();
 server.use(express.static(path.join(__dirname, 'public'), {extensions:['html']}))
+server.use(express.json())
 // server.use('/api', api)
 server.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', (err, data) => {
@@ -16,6 +17,33 @@ server.get('/api/notes', (req, res) => {
         }
     })
 })
+
+server.post('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if(err) {
+            console.error(err)
+        } else {
+            let tempDb = JSON.parse(data)
+            let {title, text} = req.body
+            let newNote = {
+                title,
+                text,
+                id: uuidv4()
+            }
+            tempDb.push(newNote)
+            fs.writeFile('./db/db.json', JSON.stringify(tempDb, null, 4), (err) => {
+                if(err) {
+                    console.error(err)
+                } else {
+                    console.log('File successfully written')
+                    res.send(newNote)
+                }
+            })
+        }
+    })
+})
+
+
 
 
 
