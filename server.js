f594e34e82bd40e9af836444017dd205203557c5
Gabriel Viernes <viernesgabriel@gmail.com>
2023-11-05 18:31:29 -0600

    added comments to server.js

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const port = 3000;
 const {v4: uuidv4} = require('uuid')
 
 const server = express();
+// middleware
 server.use(express.static(path.join(__dirname, 'public'), {extensions:['html']}))
 server.use(express.json())
 server.use(express.urlencoded({ extended: true}))
+// get route for /api/notes
 server.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', (err, data) => {
         if(err) {
@@ -17,12 +19,14 @@ server.get('/api/notes', (req, res) => {
         }
     })
 })
-
+// post route for /api/notes
 server.post('/api/notes', (req, res) => {
+    // reads the files
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if(err) {
             console.error(err)
         } else {
+            // assigns read json data to a variable then pushes data from req.body
             let tempDb = JSON.parse(data)
             let {title, text} = req.body
             let newNote = {
@@ -31,6 +35,7 @@ server.post('/api/notes', (req, res) => {
                 id: uuidv4()
             }
             tempDb.push(newNote)
+            // rewrites the db.json now with data from post req
             fs.writeFile('./db/db.json', JSON.stringify(tempDb, null, 4), (err) => {
                 if(err) {
                     console.error(err)
@@ -43,7 +48,9 @@ server.post('/api/notes', (req, res) => {
     })
 })
 
+// delete route for /api/notes/:id
 server.delete('/api/notes/:id', (req, res) => {
+    // reads file, then checks each entry for a matching id, then deletes the appropriate entry
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
             console.error(err)
@@ -55,6 +62,7 @@ server.delete('/api/notes/:id', (req, res) => {
                     break;
                 }
             }
+            // rewrites the file now minus the deleted entry
             fs.writeFile('./db/db.json', JSON.stringify(tempDb, null, 4), (err) => {
                 if(err) {
                     console.error(err)
