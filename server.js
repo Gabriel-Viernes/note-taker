d0ab29c1a26846b149ec17fdbb6a5a30e6bbd19d
Gabriel Viernes <viernesgabriel@gmail.com>
2023-11-07 17:26:40 -0600

    reworked port var to be in line with heroku's requirements

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
-const port = 3000;
+const port = process.env.PORT || 5001;
 const {v4: uuidv4} = require('uuid')
 
 const server = express();
